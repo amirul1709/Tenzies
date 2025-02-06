93cd7006d51bbb7e020717d3618c51c207c49e47
Amirul Hossain <amirul1709@hotmail.com>
2025-02-05 22:19:42 -0700

    function for random number array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,15 @@
 import Die from "./Die";
 
 export default function () {
+  function generateAllNewDice() {
+    const dice = [];
+    for (let i = 0; i < 10; i++) {
+      const ranNum = Math.ceil(Math.random() * 6);
+      dice.push(ranNum);
+    }
+    return dice;
+  }
+
   return (
     <main>
       <div className="dice-container">
