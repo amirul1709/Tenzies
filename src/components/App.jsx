aac98b0e7a43e31ba26619cb48a468f4660fed60
Amirul Hossain <amirul1709@hotmail.com>
2025-02-05 21:44:23 -0700

    update to css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,16 @@ export default function () {
   return (
     <main>
       <div className="dice-container">
+        <Die value={2} />
+        <Die value={1} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={3} />
+        <Die value={3} />
+        <Die value={2} />
         <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
-        <Die value={6} />
+        <Die value={5} />
+        <Die value={1} />
       </div>
     </main>
   );
