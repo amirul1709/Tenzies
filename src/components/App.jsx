366afb39be38a8c002d90b95b7b0790c5cedc290
Amirul Hossain <amirul1709@hotmail.com>
2025-02-08 13:33:14 -0700

    Added game description

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -53,6 +53,13 @@ export default function App() {
 
   return (
     <main>
+      <div className="game-header">
+        <h1>Tenzies</h1>
+        <p>
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls
+        </p>
+      </div>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-button" onClick={handleClick}>
         Roll
